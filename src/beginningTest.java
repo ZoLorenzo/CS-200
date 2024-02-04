7137591836ea6b0ae18bed456134a1c3d57d3197
Zo (Lorenzo) Walters <lorenzographiclaws@gmail.com>
2024-02-04 15:33:57 -0600

    first commit

---
+++ src/beginningTest.java
@@ -0,0 +1,16 @@
+public class beginningTest {
+    public static void main(String[] args) {
+        //Import the scanner class down below:
+
+        //Declare test variable:
+        int a = 12;
+
+        //Practice if statement:
+        if (a>10){
+            System.out.println("Welcome to the test!");
+        } else
+            System.out.println("You're not welcome!");
+
+    }
+    
+}
